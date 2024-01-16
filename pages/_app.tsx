a4a230d02c1ed081a3d03417c7ac7021ba273874
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-01-15 23:25:25 -0500

    build: set up palette

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
-import './globals.css';
-import type { AppProps } from 'next/app';
+import "./globals.css";
+import type { AppProps } from "next/app";
+import { Theme, injectGlobalStyles, ToastsProvider } from "@artsy/palette";
+
+const { GlobalStyles } = injectGlobalStyles(`
+  /* overrides and additions */
+`);
 
 export default function ExampleApp({
   Component,
   pageProps,
 }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <Theme>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </Theme>
+  );
 }
